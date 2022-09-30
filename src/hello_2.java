674d79a56216e56676e5c5b908e96bdd426770c8
RasputenkoED <106722996+RasputenkoED@users.noreply.github.com>
2022-09-30 15:42:22 +0500

    Update hello_2.java

--- src/hello_2.java
+++ src/hello_2.java
@@ -2,6 +2,6 @@
 public class hello_2 {
 
 	public static void main(String[] args) {
-		System.out.println("Добро\n пожаловать\n в мир\n Java!");
+		System.out.println("а=1, c=a+1 m=2, n=3 решение уравнений\n b=6, d=b*2-1 p=4, g=5 нахождение корней");
 	}
 }
